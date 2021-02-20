1c6d42fe519510c1b76b1c4d426ae346e6bfc33a
A A <ArmonH@gmail.com>
2021-02-19 23:39:11 -0500

    Add project files.

---
+++ Vidly/Controllers/TestingController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+using Vidly.Controllers;
+
+namespace Vidly.Controllers
+{
+    public class TestingController : Controller
+    {
+        // GET: Test123
+        public ActionResult Index()
+        {
+            var x = new Testing { Word = "cheesecake" };
+
+            return View(x);
+        }
+    }
+}
