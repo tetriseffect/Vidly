8f3eab3e4bf19f547e1a1fb508bdd9594bcf1e07
A A <ArmonH@gmail.com>
2021-02-22 01:14:16 -0500

    Working With Data End of Exercise 2. Added manual bday in DB. Added Details view logic

--- Vidly/Controllers/CustomerController.cs
+++ Vidly/Controllers/CustomerController.cs
@@ -32,7 +32,6 @@ namespace Vidly.Controllers
 
         public ActionResult Details(int id)
         {
-            //var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
 
