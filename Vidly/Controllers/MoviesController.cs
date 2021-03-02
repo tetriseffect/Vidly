b405157dd3c8c7fb689b3b3ccd7bab0fad38ff7f
A A <ArmonH@gmail.com>
2021-03-01 21:42:13 -0500

    Modifed MoviesController. Added Dbcontext for movies, dispose, and fixed Index()

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -6,25 +6,30 @@ using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
 using Vidly.Controllers;
+using System.Data.Entity;
+
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        public ViewResult Index()
-        {
-            var movies = GetMovies();
+        private ApplicationDbContext _context;
 
-            return View(movies);
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
         }
 
-        private IEnumerable<Movie> GetMovies()
+        protected override void Dispose(bool disposing)
         {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Shrek" },
-                new Movie { Id = 2, Name = "Wall-e" }
-            };
+            _context.Dispose();
+        }
+
+        public ViewResult Index()
+        {
+            var movies = _context.Movies.Include(c => c.Genre).ToList();
+
+            return View(movies);
         }
     }
 }
