9678aefc99de17486929cbbdb4f28744de2a4383
A A <ArmonH@gmail.com>
2021-03-03 22:57:14 -0500

    Added movie details view. Finished Exercise 3, Working with Data section

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -13,28 +13,34 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        private ApplicationDbContext _context;
+        // Set a varibale that accesses Movies in the database
+        private ApplicationDbContext _context; 
 
+        //ctor of above variable
         public MoviesController()
         {
             _context = new ApplicationDbContext();
         }
 
+        // Dispose, just gotta do this
         protected override void Dispose(bool disposing)
         {
             _context.Dispose();
         }
 
+        //Main page. The Genre inclues it on the page so you can display it in the chart
         public ViewResult Index()
         {
+            // Use _context var to access the DB
             var movies = _context.Movies.Include(c => c.Genre).ToList();
 
             return View(movies);
         }
 
+        // Details page. Include Genre since it's a seperate table
         public ActionResult Details(int id)
         {
-
+            // Use _context var to access the DB
             var movie = _context.Movies.Include(c => c.Genre).SingleOrDefault(c => c.Id == id);
 
             if (movie == null)
