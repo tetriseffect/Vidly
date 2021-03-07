fc2e4f3541070d3341e611e5e649044bfa6b5283
A A <ArmonH@gmail.com>
2021-03-07 00:16:15 -0500

    Added overrides to text in Movie controller so movieform view would be more readable

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -16,10 +16,16 @@ namespace Vidly.Models
         public string Name { get; set; }
 
         public Genre Genre { get; set; }
+
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
 
         public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Release Date")]
+
         public DateTime ReleaseDate { get; set; }
+
         public byte NumberInStock { get; set; }
 
     }
