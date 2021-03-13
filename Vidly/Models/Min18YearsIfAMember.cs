eb2e41812e53b019b4684a6c3835859b3a5df440
A A <ArmonH@gmail.com>
2021-03-12 19:37:01 -0500

    Added Custom Validation for Birthday (lecture)

---
+++ Vidly/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
+{
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+
+            if (customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1)
+                return ValidationResult.Success;
+            
+            if (customer.Birthday == null)
+                return new ValidationResult("Birthdate is required.");
+
+            var age = DateTime.Today.Year - customer.Birthday.Value.Year;
+
+            return (age >= 18)
+                ? ValidationResult.Success
+                : new ValidationResult("Customer should be at least 18 years old to go on a membership.");
+        }
+    }
+}
