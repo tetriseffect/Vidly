bd0d3ecd4700d87d428b45f7f5bbcbe0c74275c8
A A <ArmonH@gmail.com>
2021-03-28 21:17:22 -0400

    Created MoviesController API. Add GetMovie, GetMovies, and CreateMovie actions

---
+++ Vidly/Controllers/Api/MoviesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+using Vidly.Dtos;
+using AutoMapper;
+
+namespace Vidly.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET /api/movies. Get a list of movies
+        public IEnumerable<Movie> GetMovies()
+        {
+            return _context.Movies.ToList();
+        }
+
+        // GET api/movies/1. Get a single movie
+        public Movie GetMovie(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return movie;
+        }
+
+        // POST /api/customers
+        [HttpPost]
+        public Movie CreateMovie (Movie movie)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+
+            return movie;
+        }
+    }
+}
