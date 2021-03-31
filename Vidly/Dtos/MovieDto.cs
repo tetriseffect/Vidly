4468f97906c16c57c82818eab89e14d31270ac2d
A A <ArmonH@gmail.com>
2021-03-30 23:10:38 -0400

    Created Movie Dto and pasted over all properties from Movie Class

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public Genre Genre { get; set; }
+
+        [Display(Name = "Genre")]
+        [Required]
+        public byte GenreId { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Number In Stock")]
+        [Range(1, 20)]
+        public byte NumberInStock { get; set; }
+    }
+}
