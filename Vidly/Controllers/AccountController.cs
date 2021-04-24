fa384b190b3b1ca895519364100afc5045962925
tetriseffect <58157460+tetriseffect@users.noreply.github.com>
2021-04-24 00:46:03 -0400

    Added Phone to AccountController - Register. This sends phone to the db when ApplicationUser is initilized.,

--- Vidly/Controllers/AccountController.cs
+++ Vidly/Controllers/AccountController.cs
@@ -157,8 +157,10 @@ namespace Vidly.Controllers
                 {
                     UserName = model.Email,
                     Email = model.Email,
-                    DrivingLicense = model.DrivingLicense
+                    DrivingLicense = model.DrivingLicense,
+                    Phone = model.Phone
                 };
+
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
