58acaaf01522a619355c685dae8a000fd2c65013
tetriseffect <58157460+tetriseffect@users.noreply.github.com>
2021-04-24 00:35:37 -0400

    Added Phone field to Register View, then added Phone Property to the Register View Model

--- Vidly/Models/AccountViewModels.cs
+++ Vidly/Models/AccountViewModels.cs
@@ -84,6 +84,11 @@ namespace Vidly.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Phone { get; set; }
+
     }
 
     public class ResetPasswordViewModel
